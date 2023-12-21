21e362d63cc92864084ac2c8e0c247c373729a37
Hong635 <vanillanguyen635@gmail.com>
2023-12-20 20:33:35 -0800

    Update links

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 # Create a homepage view
-
-
 def home_view(request, *args, **kwargs):
     return render(request, 'home.html')
+
+# Create the about page
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html')
--- src/pdf_converter/urls.py
+++ src/pdf_converter/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # add pages
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about')
 ]

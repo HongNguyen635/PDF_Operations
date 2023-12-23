6561eb8737c3b54a02d05bbe562c112e0390206c
Hong635 <vanillanguyen635@gmail.com>
2023-12-23 12:43:55 -0800

    update db

--- src/pdf_converter/settings.py
+++ src/pdf_converter/settings.py
@@ -122,10 +122,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+# user-uploaded files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- src/pdf_manip/models.py
+++ src/pdf_manip/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UploadedFiles(models.Model):
+    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
+    def __str__(self) -> str:
+        return self.file.name
